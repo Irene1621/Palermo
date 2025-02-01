8c85a5edb35bf474bd211dea36ed04466f8843b1
Irene ka <irenekatramidi@gmail.com>
2025-02-01 11:45:53 +0200

    Added some useful methods in the class Player

--- src/Palermo.Domain/Models/Player.cs
+++ src/Palermo.Domain/Models/Player.cs
@@ -16,6 +16,7 @@ namespace Palermo.Models
         public string Name { get; set; }
         public bool IsAlive { get; set; }
         public RoleType Role { get; set; }
+        public int Votes { get; set; }
 
         public abstract void PerformNightAction(Game game);
 
@@ -23,5 +24,28 @@ namespace Palermo.Models
         {
             //Allows the player to receive information
         }
+
+        public void AddVote(Player player)
+        {
+            player.Votes++;
+        }
+
+        public void EliminatePlayer(Player player)
+        {
+            player.IsAlive = false;
+        }
+
+        public void Vote(Player voter, Player target )
+        {
+            AddVote(target);
+            voter.HasPlayerVoted = true;
+        }
+
+        public void ResetVotes(Player player)
+        {
+            player.Votes = 0;
+        }
+
+        public bool HasPlayerVoted {  get; set; }
     }
 }
