7deae07f99327a834159fc50b975ec33dd30f6c8
Irene ka <irenekatramidi@gmail.com>
2025-02-04 20:05:58 +0200

    Developed the VotingResult class and made a few changes in the VotingService class

---
+++ src/Palermo.Domain/Services/VotingResult.cs
@@ -0,0 +1,44 @@
+﻿using Palermo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palermo.Domain.Services
+{
+    internal static class VotingResult
+    {
+        
+        public static Player GetVotingResults(List<Player> players)
+        {
+            
+            var alivePlayers = players.Where(x => x.IsAlive == true).ToList();
+
+            if (alivePlayers.Count == VotingService.PlayersVoted.Count)
+            {
+                var eliminatedPlayer = GetEliminatedPlayer(players);
+                foreach (var player in players)
+                {
+                    player.ResetVotes();
+                }
+                return eliminatedPlayer;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static Player GetEliminatedPlayer(List<Player> players)
+        {
+            //Determines the player with the most votes.
+            for (int i = 0; i < players.Count; i++)
+            {
+                var player = players[i];
+                players.OrderByDescending(x => x.Votes);
+            }
+            return players[0];
+        }
+    }
+}
--- src/Palermo.Domain/Services/VotingService.cs
+++ src/Palermo.Domain/Services/VotingService.cs
@@ -12,39 +12,17 @@ namespace Palermo.Domain.Services
     public static class VotingService
     {
         public static List<Player> PlayersVoted { get; set; } = [];
-        public static Player? StartVotingService(Player voter, Player target, List<Player> players)
+        public static void StartVotingService(Player voter, Player target, List<Player> players)
         {
             voter.Vote(target);
 
-            var playersVoted = players.Where(x => x.HasPlayerVoted = true).ToList();
+            var playersVoted = players.Where(x => x.HasPlayerVoted == true).ToList();
             foreach (var player in playersVoted)
             {
                 PlayersVoted.Add(player);
             }
 
-          if (players.Count == PlayersVoted.Count)
-            {
-               var eliminatedPlayer = GetEliminatedPlayer(players);
-               foreach (var player in players) 
-                {
-                    player.ResetVotes();
-                }
-               return eliminatedPlayer;
-            }
-          else
-            {
-                return null;
-            }
         }
-        public static Player GetEliminatedPlayer(List<Player> players)
-        {
-            //Determines the player with the most votes.
-            for (int i = 0; i < players.Count; i++)
-            {
-                var player = players[i];
-                players.OrderByDescending(x => x.Votes);
-            }
-            return players[0];
-        }
+        
     } 
 }
